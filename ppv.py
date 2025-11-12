c11c059069cb9a7dce9c7de3c0d348e643b33824
zRzJS <53818328+zRzJS@users.noreply.github.com>
2025-11-12 14:28:52 +0800

    Update ppv.py

--- ppv.py
+++ ppv.py
@@ -5,57 +5,22 @@ from datetime import datetime
 
 API_URL = "https://ppv.to/api/streams"
 
-CUSTOM_HEADERS = [
-    '#EXTVLCOPT:http-origin=https://ppv.to',
-    '#EXTVLCOPT:http-referrer=https://ppv.to/',
-    '#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:143.0) Gecko/20100101 Firefox/143.0'
-]
+CUSTOM_HEADERS = []
 
 ALLOWED_CATEGORIES = {
-    "24/7 Streams", "Wrestling", "Football", "Basketball", "Baseball",
-    "Combat Sports", "American Football", "Darts", "Motorsports", "Ice Hockey"
+    "Basketball"
 }
 
 CATEGORY_LOGOS = {
-    "24/7 Streams": "http://drewlive24.duckdns.org:9000/Logos/247.png",
-    "Wrestling": "http://drewlive24.duckdns.org:9000/Logos/Wrestling.png",
-    "Football": "http://drewlive24.duckdns.org:9000/Logos/Football.png",
-    "Basketball": "http://drewlive24.duckdns.org:9000/Logos/Basketball.png",
-    "Baseball": "http://drewlive24.duckdns.org:9000/Logos/Baseball.png",
-    "American Football": "http://drewlive24.duckdns.org:9000/Logos/NFL3.png",
-    "Combat Sports": "http://drewlive24.duckdns.org:9000/Logos/CombatSports2.png",
-    "Darts": "http://drewlive24.duckdns.org:9000/Logos/Darts.png",
-    "Motorsports": "http://drewlive24.duckdns.org:9000/Logos/Motorsports2.png",
-    "Live Now": "http://drewlive24.duckdns.org:9000/Logos/DrewLiveSports.png",
-    "Ice Hockey": "http://drewlive24.duckdns.org:9000/Logos/Hockey.png"
+    "Basketball": "http://drewlive24.duckdns.org:9000/Logos/Basketball.png"
 }
 
 CATEGORY_TVG_IDS = {
-    "24/7 Streams": "24.7.Dummy.us",
-    "Wrestling": "PPV.EVENTS.Dummy.us",
-    "Football": "Soccer.Dummy.us",
-    "Basketball": "Basketball.Dummy.us",
-    "Baseball": "MLB.Baseball.Dummy.us",
-    "American Football": "NFL.Dummy.us",
-    "Combat Sports": "PPV.EVENTS.Dummy.us",
-    "Darts": "Darts.Dummy.us",
-    "Motorsports": "Racing.Dummy.us",
-    "Live Now": "24.7.Dummy.us",
-    "Ice Hockey": "NHL.Hockey.Dummy.us"
+"Basketball": "Basketball.Dummy.us"
 }
 
 GROUP_RENAME_MAP = {
-    "24/7 Streams": "PPVLand - Live Channels 24/7",
-    "Wrestling": "PPVLand - Wrestling Events",
-    "Football": "PPVLand - Global Football Streams",
-    "Basketball": "PPVLand - Basketball Hub",
-    "Baseball": "PPVLand - MLB",
-    "American Football": "PPVLand - NFL Action",
-    "Combat Sports": "PPVLand - Combat Sports",
-    "Darts": "PPVLand - Darts",
-    "Motorsports": "PPVLand - Racing Action",
-    "Live Now": "PPVLand - Live Now",
-    "Ice Hockey": "PPVLand - NHL Action"
+    "Basketball": "PPVLand - Basketball Hub"
 }
 
 NFL_TEAMS = {
